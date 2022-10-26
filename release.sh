078af70bd36a10afc7c88c20c7fb0a6f4f50a573
zhangub <112906505@qq.com>
2022-10-26 10:05:47 +0800

    test

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 git add .
 git commit -m "test"
 git push origin master
-git tag -a v0.3.15 -m "test release"
-goreleaser release  --rm-dist
+git tag -a v0.3.16 -m "test release"
+# goreleaser release  --rm-dist
+goreleaser --snapshot --skip-publish --rm-dist
