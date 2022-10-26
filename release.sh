eb3d8ef1ea9d80315e443dc740707eb7318f5ba1
zhangub <112906505@qq.com>
2022-10-26 11:36:03 +0800

    testcommit

--- release.sh
+++ release.sh
@@ -1,6 +1,11 @@
 git add .
-git commit -m "test"
+# testcommit: your commite message
+git commit -m "testcommit"
+# master: your branch name 
 git push origin master
+# vXX.XX.XX: your version. your tag
 git tag -a v0.3.17 -m "test release"
+# release and push to github & docker.io
 goreleaser release  --rm-dist
+# only test realase
 # goreleaser --snapshot --skip-publish --rm-dist
