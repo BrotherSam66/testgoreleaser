f77f9de2216e084e45743988481ea3efdd5da59f
zhangub <112906505@qq.com>
2022-11-03 22:28:40 +0800

    testactions

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@ git add .
 # testcommit: your commite message
 git commit -m "testactions"
 # vXX.XX.XX: your version. your tag
-git tag -a v0.3.97 -m "test release"
+git tag -a v0.3.98 -m "test release"
 # master: your branch name 
 git push origin master
 # release and push to github & docker.io
